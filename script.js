f8d924a9eb7a5de5497ffb073bc8fe40f92e449a
HarryFolkard <folkardharry@gmail.com>
2022-10-25 15:56:29 +0100

    Adding functionality via JavaScript

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
-const readMoreBtn = document.querySelector(".read-more-btn");
-const text = document.querySelector(".text");
-
-readMoreBtn.addEventListener("click", (e) => {
-  text.classList.toggle("show-more");
-  if (readMoreBtn.innerText === "Read More") {
-    readMoreBtn.innerText = "Read Less";
-  } else {
-    readMoreBtn.innerText = "Read More";
+// READ MORE BTNs
+function btnSelection(c) {
+  var y = document.querySelectorAll(".moretext");
+  var z = document.querySelectorAll(".btn");
+  console.log(z);
+  for (i = 0 ; i < y.length ; i++) {
+    if (y[i].className.split(" ").indexOf(c) > -1) {
+      y[i].classList.toggle("show");
+      if (z[i].innerText === "Read More") {
+        z[i].innerText = "Read Less";
+      } else {
+        z[i].innerText = "Read More";
+      }
+    }
   }
-});
+}
+
+// TAGS
+function tagSelection(c) {
+  var x = document.querySelectorAll(".all");
+  for (i = 0 ; i < x.length ; i++) {
+    if (x[i].className.split(" ").indexOf(c) > -1) {
+      addClass(x[i], "show");
+    } else {
+      addClass(x[i], "hide");
+    }
+  }
+}
+
+function addClass(element, name) {
+  var arr1 = element.className.split(" ");
+  var arr2 = name;
+  checkAndRemove(arr1, element);
+  element.className += " " + arr2;
+}
+
+function checkAndRemove(input, element) {
+  var arry1 = input;
+  if (arry1.indexOf("show") > -1) {
+    element.classList.remove("show");
+  }
+  if (arry1.indexOf("hide") > -1) {
+    element.classList.remove("hide");
+  }
+  return element;
+}
+
+
+
+
+
+
